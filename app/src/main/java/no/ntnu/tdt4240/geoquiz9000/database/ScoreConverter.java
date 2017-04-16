76e2dd5a80a4ab9f0bd017a7c8345696b0d063e7
Alexander Fischer <alex.fi93@gmail.com>
2017-04-16 12:46:15 +0200

    Make some important fixes, refactoring and finish TODO's.

---
+++ app/src/main/java/no/ntnu/tdt4240/geoquiz9000/database/ScoreConverter.java
@@ -0,0 +1,40 @@
+package no.ntnu.tdt4240.geoquiz9000.database;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.objectbox.converter.PropertyConverter;
+
+/**
+ * Class for converting float list to String to make it compatible with ObjectBox.
+ */
+public class ScoreConverter implements PropertyConverter<List<Float>, String> {
+    private static final String TAG = ScoreConverter.class.getSimpleName();
+
+    @Override
+    public List<Float> convertToEntityProperty(String s) {
+        List<Float> dists = new ArrayList<>();
+        try {
+            JSONArray jsonArray = new JSONArray(s);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                String jsonValue = jsonArray.opt(i).toString();
+                dists.add(Float.valueOf(jsonValue));
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Not possible to convert String to List<Float> again");
+        }
+
+        return dists;
+    }
+
+    @Override
+    public String convertToDatabaseValue(List<Float> floats) {
+        JSONArray jsonArray = new JSONArray(floats);
+        return jsonArray.toString();
+    }
+}
--- app/src/main/java/no/ntnu/tdt4240/geoquiz9000/models/Score.java
+++ app/src/main/java/no/ntnu/tdt4240/geoquiz9000/models/Score.java
@@ -3,10 +3,6 @@ package no.ntnu.tdt4240.geoquiz9000.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
-import android.util.Log;
-
-import org.json.JSONArray;
-import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +11,7 @@ import io.objectbox.annotation.Convert;
 import io.objectbox.annotation.Entity;
 import io.objectbox.annotation.Generated;
 import io.objectbox.annotation.Id;
-import io.objectbox.converter.PropertyConverter;
+import no.ntnu.tdt4240.geoquiz9000.database.ScoreConverter;
 
 @Entity
 public class Score extends AbstractModel implements Parcelable, Comparable<Score> {
@@ -61,6 +57,23 @@ public class Score extends AbstractModel implements Parcelable, Comparable<Score
         this.distances = distances;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public void setMapPackName(String mapPackName) {
+        this.mapPackName = mapPackName;
+    }
+
+
     protected Score(Parcel in) {
         playerName = in.readString();
         totalDistance = in.readFloat();
@@ -75,7 +88,7 @@ public class Score extends AbstractModel implements Parcelable, Comparable<Score
 
     @Generated(hash = 689965711)
     public Score(long id, String playerName, float totalDistance, List<Float> distances,
-            String mapPackName) {
+                 String mapPackName) {
         this.id = id;
         this.playerName = playerName;
         this.totalDistance = totalDistance;
@@ -105,6 +118,7 @@ public class Score extends AbstractModel implements Parcelable, Comparable<Score
         dest.writeString(mapPackName);
     }
 
+    // Make Score parcelable so that it can be passed through intents.
     @SuppressWarnings("unused")
     public static final Parcelable.Creator<Score> CREATOR = new Parcelable.Creator<Score>() {
         @Override
@@ -127,52 +141,5 @@ public class Score extends AbstractModel implements Parcelable, Comparable<Score
             return this.totalDistance > score.totalDistance ? 1 : -1;
         }
 
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
-    }
-
-    public void setMapPackName(String mapPackName) {
-        this.mapPackName = mapPackName;
-    }
-
-    public static class ScoreConverter implements PropertyConverter<List<Float>, String> {
-        private static final String TAG = ScoreConverter.class.getSimpleName();
-
-        @Override
-        public List<Float> convertToEntityProperty(String s) {
-            List<Float> dists = new ArrayList<>();
-            try {
-                JSONArray jsonArray = new JSONArray(s);
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    String jsonValue = jsonArray.opt(i).toString();
-                    dists.add(Float.valueOf(jsonValue));
-                }
-            } catch (JSONException e) {
-                Log.e(TAG, "Not possible to convert String to List<Float> again");
-            }
-
-            for (float dist: dists) {
-                Log.d(TAG, String.valueOf(dist));
-            }
-
-            return dists;
-        }
-
-        @Override
-        public String convertToDatabaseValue(List<Float> floats) {
-            JSONArray jsonArray = new JSONArray(floats);
-            Log.d(TAG, jsonArray.toString());
-            return jsonArray.toString();
-        }
     }
 }
