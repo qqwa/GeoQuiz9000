ac77ba07d3f6255210bb421e5ce6ce39c7551083
Alexander Fischer <alex.fi93@gmail.com>
2017-04-17 12:54:51 +0200

    Final commit for image activity.

--- app/src/main/java/no/ntnu/tdt4240/geoquiz9000/ui/PinView.java
+++ app/src/main/java/no/ntnu/tdt4240/geoquiz9000/ui/PinView.java
@@ -10,6 +10,10 @@ import android.util.AttributeSet;
 
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import no.ntnu.tdt4240.geoquiz9000.R;
 
 /**
@@ -17,8 +21,14 @@ import no.ntnu.tdt4240.geoquiz9000.R;
  */
 public class PinView extends SubsamplingScaleImageView {
 
+    public enum Colors {
+        GREEN, RED
+    }
+
+    private Map<PointF, Colors> sPoints = new HashMap<>();
+    private Map<PointF, Bitmap> pins = new HashMap<>();
     private PointF sPin;
-    private Bitmap pin;
+    private Bitmap clearPin;
 
     public PinView(Context context) {
         this(context, null);
@@ -26,11 +36,48 @@ public class PinView extends SubsamplingScaleImageView {
 
     public PinView(Context context, AttributeSet attr) {
         super(context, attr);
-        initialise();
     }
 
+    /**
+     * Set one pin on the map.
+     *
+     * @param sPin point of the pin
+     */
     public void setPin(PointF sPin) {
         this.sPin = sPin;
+        if (sPoints == null) sPoints = new HashMap<>();
+        if (pins == null) pins = new HashMap<>();
+
+        sPoints.put(sPin, Colors.RED);
+        initialise();
+        invalidate();
+    }
+
+    /**
+     * Set one solution pin on the map.
+     *
+     * @param sPin point of the pin
+     */
+    public void setSolutionPin(PointF sPin) {
+        this.sPin = sPin;
+        if (sPoints == null) sPoints = new HashMap<>();
+        if (pins == null) pins = new HashMap<>();
+
+        sPoints.put(sPin, Colors.GREEN);
+        initialise();
+        invalidate();
+    }
+
+    /**
+     * Set a map of pins on the map.
+     *
+     * @param sPoints Map with location and color information
+     */
+    public void setPins(Map<PointF, Colors> sPoints) {
+        if (sPoints == null) sPoints = new HashMap<>();
+        if (pins == null) pins = new HashMap<>();
+
+        this.sPoints = sPoints;
         initialise();
         invalidate();
     }
@@ -39,18 +86,54 @@ public class PinView extends SubsamplingScaleImageView {
         return sPin;
     }
 
+    /**
+     * Reset the map from pins.
+     */
     public void clear() {
+        clearPin = null;
         sPin = null;
+        sPoints = null;
+        pins = null;
+
         initialise();
         invalidate();
     }
 
     private void initialise() {
-        float density = getResources().getDisplayMetrics().densityDpi;
-        pin = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_place_red);
-        float w = (density/420f) * pin.getWidth();
-        float h = (density/420f) * pin.getHeight();
-        pin = Bitmap.createScaledBitmap(pin, (int)w, (int)h, true);
+        if (sPoints != null && sPoints.size() != 0) {
+            Set<PointF> pointFs = sPoints.keySet();
+
+            for (PointF p : pointFs) {
+                Bitmap pin;
+                float density = getResources().getDisplayMetrics().densityDpi;
+
+                Colors c = sPoints.get(p);
+
+                switch (c) {
+                    case RED:
+                        pin = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_place_red);
+                        break;
+                    case GREEN:
+                        pin = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_action_name);
+                        break;
+                    default:
+                        pin = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_place_red);
+                }
+
+                float w = (density / 420f) * pin.getWidth();
+                float h = (density / 420f) * pin.getHeight();
+                pin = Bitmap.createScaledBitmap(pin, (int) w, (int) h, true);
+
+                pins.put(p, pin);
+            }
+        } else if (sPin == null) {
+            float density = getResources().getDisplayMetrics().densityDpi;
+            clearPin = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_place_red);
+            float w = (density / 420f) * clearPin.getWidth();
+            float h = (density / 420f) * clearPin.getHeight();
+            clearPin = Bitmap.createScaledBitmap(clearPin, (int) w, (int) h, true);
+
+        }
     }
 
     @Override
@@ -65,13 +148,19 @@ public class PinView extends SubsamplingScaleImageView {
         Paint paint = new Paint();
         paint.setAntiAlias(true);
 
-        if (sPin != null && pin != null) {
-            PointF vPin = sourceToViewCoord(sPin);
-            float vX = vPin.x - (pin.getWidth()/2);
-            float vY = vPin.y - pin.getHeight();
-            canvas.drawBitmap(pin, vX, vY, paint);
-        }
+        if (pins != null) {
+            Set<PointF> pointFs = sPoints.keySet();
 
+            for (PointF p : pointFs) {
+                Bitmap pin = pins.get(p);
+
+                PointF vPin = sourceToViewCoord(p);
+                float vX = vPin.x - (pin.getWidth() / 2);
+                float vY = vPin.y - pin.getHeight();
+                canvas.drawBitmap(pin, vX, vY, paint);
+            }
+
+        }
     }
 
 }
