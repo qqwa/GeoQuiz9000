ae4d6e630015dc76c39e695306183c3070532ccf
Mikhail Vasilyev <mikail.vasilyev@gmail.com>
2017-04-21 18:09:06 +0200

    Default maps import done, not tested

--- app/src/main/java/no/ntnu/tdt4240/geoquiz9000/App.java
+++ app/src/main/java/no/ntnu/tdt4240/geoquiz9000/App.java
@@ -1,22 +1,31 @@
 package no.ntnu.tdt4240.geoquiz9000;
 
 import android.app.Application;
+import android.os.AsyncTask;
 
+import io.objectbox.Box;
 import io.objectbox.BoxStore;
+import no.ntnu.tdt4240.geoquiz9000.controllers.AsyncImportMap;
+import no.ntnu.tdt4240.geoquiz9000.controllers.MapFactory;
+import no.ntnu.tdt4240.geoquiz9000.database.DatabaseLayer;
+import no.ntnu.tdt4240.geoquiz9000.models.MapStore;
 import no.ntnu.tdt4240.geoquiz9000.models.MyObjectBox;
 
-public class App extends Application {
+public class App extends Application
+{
 
     private BoxStore mBoxStore;
 
     @Override
-    public void onCreate() {
+    public void onCreate()
+    {
         super.onCreate();
 
         mBoxStore = MyObjectBox.builder().androidContext(App.this).build();
     }
 
-    public BoxStore getBoxStore() {
+    public BoxStore getBoxStore()
+    {
         return mBoxStore;
     }
 
